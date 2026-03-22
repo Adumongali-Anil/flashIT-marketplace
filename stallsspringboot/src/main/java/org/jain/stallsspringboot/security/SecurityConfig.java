31b89e45541f002fde76d8d086f1bef55f2dccbe
Anil Kumar <adumongalianilkumar@gmail.com>
2026-03-22 17:37:45 +0530

    fix security root update access

--- stallsspringboot/src/main/java/org/jain/stallsspringboot/security/SecurityConfig.java
+++ stallsspringboot/src/main/java/org/jain/stallsspringboot/security/SecurityConfig.java
@@ -89,7 +89,10 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+    "http://localhost:3000",
+    "https://flashit-marketplace.onrender.com"
+));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
