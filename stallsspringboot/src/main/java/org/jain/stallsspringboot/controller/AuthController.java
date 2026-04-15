542d65b601cd5468224c9c9070761d5bd6f886fc
Adumongali-Anil <adumongalianilkumar@gmail.com>
2026-04-15 13:45:28 +0530

    Fix AuthController imports: add UsernamePasswordAuthenticationToken import

--- stallsspringboot/src/main/java/org/jain/stallsspringboot/controller/AuthController.java
+++ stallsspringboot/src/main/java/org/jain/stallsspringboot/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.jain.stallsspringboot.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
